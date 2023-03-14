4fd6886c6c18408a9c32bc44143c2fa315cda9f0
mahmoud <eslamimahmoud98@gmail.com>
2023-03-14 07:06:56 +0330

    feat: add portfolio data

---
+++ portfolio/portfolio.dart
@@ -0,0 +1,273 @@
+const baseUrl = 
+"https://raw.githubusercontent.com/mahmoud-eslami/resume/main";
+var dataset = {
+  "projects": [
+    {
+      "title": "Pmodoro",
+      "description":
+          "An application that helps users implement the Pomodoro 
+Technique.",
+      "banner": "$baseUrl/portfolio/pomodoro_banner.png",
+      "images": [],
+      "isOpenSource": true,
+      "link": "https://github.com/time-mastery/pmodoro-application",
+      "technologies": [
+        "Flutter",
+        "Dart",
+        "BloC (state management)",
+        "get it (dependency injection)",
+        "Sqflite (offline database)"
+      ],
+    },
+    {
+      "title": "Ketolife",
+      "description": "A keto diet tracking and information app.",
+      "banner": "$baseUrl/portfolio/ketolife_banner.png",
+      "images": [
+        "$baseUrl/images/ketolife/27.png",
+        "$baseUrl/images/ketolife/28.png",
+        "$baseUrl/images/ketolife/20.png",
+        "$baseUrl/images/ketolife/21.png",
+        "$baseUrl/images/ketolife/22.png",
+        "$baseUrl/images/ketolife/16.png",
+        "$baseUrl/images/ketolife/17.png",
+        "$baseUrl/images/ketolife/23.png",
+        "$baseUrl/images/ketolife/24.png",
+        "$baseUrl/images/ketolife/1.png",
+        "$baseUrl/images/ketolife/2.png",
+        "$baseUrl/images/ketolife/3.png",
+        "$baseUrl/images/ketolife/4.png",
+        "$baseUrl/images/ketolife/5.png",
+        "$baseUrl/images/ketolife/6.png",
+        "$baseUrl/images/ketolife/7.png",
+        "$baseUrl/images/ketolife/8.png",
+        "$baseUrl/images/ketolife/9.png",
+        "$baseUrl/images/ketolife/10.png",
+        "$baseUrl/images/ketolife/11.png",
+        "$baseUrl/images/ketolife/12.png",
+        "$baseUrl/images/ketolife/13.png",
+        "$baseUrl/images/ketolife/14.png",
+        "$baseUrl/images/ketolife/15.png",
+        "$baseUrl/images/ketolife/18.png",
+        "$baseUrl/images/ketolife/19.png",
+      ],
+      "isOpenSource": false,
+      "link": "https://",
+      "technologies": [
+        "Flutter",
+        "Dart",
+        "BloC (state management)",
+        "get it (dependency injection)",
+        "Sqflite (offline database)"
+      ],
+    },
+    {
+      "title": "Codehub",
+      "description":
+          "CodeHub is a Persian pastebin service that provides all kind of 
+software services completely Free and Open Source.",
+      "banner": "$baseUrl/portfolio/codehub_banner.png",
+      "images": [
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/13.png",
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/14.png",
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/15.png",
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/16.png",
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/17.png",
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/18.png",
+        
+"https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/19.png",
+      ],
+      "isOpenSource": true,
+      "link": "https://github.com/codehub-ir/codehub-mobile/",
+      "technologies": [
+        "Flutter",
+        "Dart",
+        "BloC (state management)",
+        "get it (dependency injection)",
+      ],
+    },
+    {
+      "title": "Barber booking",
+      "description":
+          "A barber shop booking platform using Firebase as the backend 
+service and Flutter as the client technology.",
+      "banner": "$baseUrl/portfolio/barber_banner.png",
+      "images": [
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/1.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/2.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/3.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/4.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/5.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/6.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/7.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/8.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/9.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/10.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/11.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/12.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/13.png",
+      ],
+      "isOpenSource": true,
+      "link": "https://github.com/mahmoud-eslami/barber-booking/",
+      "technologies": [
+        "Flutter",
+        "Dart",
+        "Firebase",
+        "Firebase Analytics",
+        "Firebase Firesotre",
+        "Get-X (state management / dependency injection)",
+      ],
+    },
+    {
+      "title": "Meal planners",
+      "description":
+          "A desktop app that helps users find recipes with detailed 
+information.",
+      "banner": "$baseUrl/portfolio/meal_planer.png",
+      "images": [
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-1.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-2.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-3.png",
+        
+"https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-4.png",
+      ],
+      "isOpenSource": false,
+      "link": "https://",
+      "technologies": [
+        "Flutter",
+        "Flutter-Desktop",
+        "Dart",
+        "Get-X (state management / dependency injection)",
+      ],
+    }
+  ],
+  "skills": [
+    {
+      "title": "Main skills",
+      "description": "",
+      "skillItems": [
+        {
+          "title": "Flutter",
+          "long": false,
+          "image": "$baseUrl/portfolio/fl_logo.png",
+        },
+        {
+          "title": "Dart",
+          "long": false,
+          "image": "$baseUrl/portfolio/dart_logo.png",
+        },
+        {
+          "title": "BLoC Pattern",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "Dio & Http",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "i18n & l10n",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "Animations",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "Asynchronous programming",
+          "long": true,
+          "image": null,
+        },
+      ]
+    },
+    {
+      "title": "Software development skills",
+      "description": "",
+      "skillItems": [
+        {
+          "title": "OOP",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "Git",
+          "long": false,
+          "image": "$baseUrl/portfolio/git_logo.png",
+        },
+        {
+          "title": "CI / CD",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "MVVM",
+          "long": false,
+          "image": null,
+        },
+        {
+          "title": "Github actions",
+          "long": false,
+          "image": null,
+        },
+      ]
+    },
+    {
+      "title": "Design skills",
+      "description": "",
+      "skillItems": [
+        {
+          "title": "Material Design",
+          "long": true,
+          "image": "$baseUrl/portfolio/material_logo.png",
+        },
+        {
+          "title": "Cupertino",
+          "long": false,
+          "image": null,
+        },
+      ]
+    },
+    {
+      "title": "Familiar with",
+      "description": "",
+      "skillItems": [
+        {
+          "title": "Python",
+          "long": false,
+          "image": "$baseUrl/portfolio/python_lang_logo.png",
+        },
+        {
+          "title": "Django",
+          "long": false,
+          "image": "$baseUrl/portfolio/django_logo.png",
+        },
+      ]
+    },
+  ],
+};
+
